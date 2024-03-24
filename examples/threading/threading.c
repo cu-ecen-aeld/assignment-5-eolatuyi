2e24ad57299b121b1b03c7f10c11512de144a91f
eolatuyi <ebenezer@olatuyi.com>
2024-03-24 02:54:05 +0000

    Using Pthread for synch.

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -8,12 +8,31 @@
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
+typedef struct thread_data thread_data_t;
+
 void* threadfunc(void* thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_data_t *thread_func_args = (thread_data_t *)thread_param;
+
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if (pthread_mutex_lock(thread_func_args->mutex) != 0) {
+        thread_func_args->thread_complete_success = false;
+        return thread_func_args;
+    }
+
+    usleep(thread_func_args->wait_to_release_ms * 1000);
+
+    if (pthread_mutex_unlock(thread_func_args->mutex) != 0) {
+        thread_func_args->thread_complete_success = false;
+        return thread_func_args;
+    }
+
+    thread_func_args->thread_complete_success = true;
+	
     return thread_param;
 }
 
@@ -28,6 +47,21 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    thread_data_t *data = malloc(sizeof(thread_data_t));
+    if (data == NULL) {
+        return false;
+    }
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+
+    int result = pthread_create(thread, NULL, threadfunc, data);
+    if (result != 0) {
+        free(data);
+        return false;
+    }
+
+    return true;
 }
 
