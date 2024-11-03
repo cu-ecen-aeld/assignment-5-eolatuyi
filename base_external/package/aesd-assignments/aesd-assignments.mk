65a632127ef3c563e72f4b66b3c903a6d4480c8a
eolatuyi <ebenezer@olatuyi.com>
2024-11-03 04:10:26 +0000

    integrate package installation metadata

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 9bc7a475e01bf2e0592a5af9e0ffbf648b297f0e
+AESD_ASSIGNMENTS_VERSION = 2653fdc1bf14e9eafe809edc46a6c4f98846db3d
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-eolatuyi.git
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignment-5-eolatuyi.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
